01eb38da454aa5d92253714c6df02db3709c97f3
FliegendeWurst <2012gdwu@web.de>
2016-08-05 11:50:04 +0200

    1st Example

---
+++ 1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace open_btd
+{
+	class Program
+	{
+		[STAThread]
+		public static void Main()
+		{
+
+			using (var Game = new GameWindow())
+			{
+				int ProgramID = 0;
+
+				int VertexBuffer = 0;
+				
+				string VertexShaderFilePath = "vs.glsl";
+				string FragmentShaderFilePath = "fs.glsl";
+
+				Game.Load += (sender, e) =>
+				{
+					GL.ClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+					// setup settings, load textures, sounds
+					int VertexArrayID;
+					GL.GenVertexArrays(1, out VertexArrayID);
+					GL.BindVertexArray(VertexArrayID);
+
+					ProgramID = Util.LoadShaders(VertexShaderFilePath,
+						FragmentShaderFilePath);
+
+					Vector2[] vertices = new Vector2[4];
+					vertices[0] = new Vector2(-0.5f, 0.5f);
+					vertices[1] = new Vector2(-0.5f, -0.5f);
+					vertices[2] = new Vector2(0.5f, -0.5f);
+					vertices[3] = new Vector2(0.5f, 0.5f);
+
+					GL.GenBuffers(1, out VertexBuffer);
+					GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBuffer);
+					GL.BufferData(BufferTarget.ArrayBuffer,
+						new IntPtr(vertices.Length * Vector2.SizeInBytes),
+						vertices, BufferUsageHint.StaticDraw);
+
+					GL.UseProgram(ProgramID);
+
+					Game.VSync = VSyncMode.On;
+				};
+
+				Game.Resize += (sender, e) =>
+				{
+					GL.Viewport(0, 0, Game.Width, Game.Height);
+				};
+
+				Game.UpdateFrame += (sender, e) =>
+				{
+					// add game logic, input handling
+					if (Keyboard.GetState().IsKeyDown(Key.Escape))
+					{
+						Game.Exit();
+					}
+				};
+
+				Game.RenderFrame += (sender, e) =>
+				{
+					GL.Clear(ClearBufferMask.ColorBufferBit);
+
+					GL.EnableVertexAttribArray(0);
+					GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBuffer);
+					GL.VertexAttribPointer(
+						0,
+						2,
+						VertexAttribPointerType.Float,
+						false,
+						0,
+						0
+					);
+
+					GL.DrawArrays(PrimitiveType.Quads, 0, 4);
+					GL.DisableVertexAttribArray(0);
+
+					Game.SwapBuffers();
+				};
+
+				// Run the game at 60 updates per second
+				Game.Run(60.0);
+			}
+		}
+	}
+}
---
+++ 1/Util.cs
@@ -0,0 +1,77 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System;
+using System.Text;
+
+namespace open_btd
+{
+	class Util
+	{
+		public static int LoadShaders(string VertexFilePath, string FragmentFilePath)
+		{
+			int ProgramID = GL.CreateProgram();
+
+			// Create the shaders
+			int VertexShaderID = GL.CreateShader(ShaderType.VertexShader);
+			int FragmentShaderID = GL.CreateShader(ShaderType.FragmentShader);
+
+			// Read the Vertex Shader code from the file
+			string VertexShaderCode = System.IO.File.ReadAllText(VertexFilePath);
+
+			// Read the Fragment Shader code from the file
+			string FragmentShaderCode = System.IO.File.ReadAllText(FragmentFilePath);
+
+			// Compile Vertex Shader
+			GL.ShaderSource(VertexShaderID, VertexShaderCode);
+			GL.CompileShader(VertexShaderID);
+
+			int Result;
+			int InfoLogLength;
+
+			// Check Vertex Shader
+			GL.GetShader(VertexShaderID, ShaderParameter.CompileStatus, out Result);
+			GL.GetShader(VertexShaderID, ShaderParameter.InfoLogLength, out InfoLogLength);
+			if (InfoLogLength > 1)
+			{
+				Console.WriteLine("Compiling vertex shader..");
+				StringBuilder SB = new StringBuilder();
+				GL.GetShaderInfoLog(VertexShaderID, InfoLogLength, out InfoLogLength, SB);
+				Console.WriteLine("Compile status: " + (Result != 0));
+				Console.Write(SB.ToString());
+			}
+
+			// Compile Fragment Shader
+
+			GL.ShaderSource(FragmentShaderID, FragmentShaderCode);
+			GL.CompileShader(FragmentShaderID);
+
+			Result = 0;
+			InfoLogLength = 0;
+
+			// Check Fragment Shader
+			GL.GetShader(VertexShaderID, ShaderParameter.CompileStatus, out Result);
+			GL.GetShader(VertexShaderID, ShaderParameter.InfoLogLength, out InfoLogLength);
+			if (InfoLogLength > 1)
+			{
+				Console.WriteLine("Compiling fragment shader..");
+				StringBuilder sb = new StringBuilder();
+				GL.GetShaderInfoLog(FragmentShaderID, InfoLogLength, out InfoLogLength, sb);
+				Console.WriteLine("Compile status: " + (Result != 0));
+				Console.Write(sb.ToString());
+			}
+
+			// Link the program
+			GL.AttachShader(ProgramID, VertexShaderID);
+			GL.AttachShader(ProgramID, FragmentShaderID);
+			GL.LinkProgram(ProgramID);
+
+			GL.DetachShader(ProgramID, VertexShaderID);
+			GL.DetachShader(ProgramID, FragmentShaderID);
+
+
+			GL.DeleteShader(VertexShaderID);
+			GL.DeleteShader(FragmentShaderID);
+
+			return ProgramID;
+		}
+	}
+}
